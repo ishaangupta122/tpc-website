0cdd739c9b0ec4bbbd9af6994ba25e01074a14b5
harmanbatheja15 <harmanbatheja15@gmail.com>
2025-02-06 10:42:33 +0530

    refactor code

--- frontend/src/components/TruncateText.jsx
+++ frontend/src/components/TruncateText.jsx
@@ -4,11 +4,11 @@ const TruncateText = ({ text, maxLength = 100 }) => {
 	const [isTruncated] = useState(true);
 
 	return (
-		<div>
-			<p>
-				{isTruncated ? text.slice(0, maxLength) + (text.length > maxLength ? '...' : '') : text}
-			</p>
-		</div>
+		<>
+			{isTruncated
+				? text.slice(0, maxLength) + (text.length > maxLength ? '...' : '')
+				: text}
+		</>
 	);
 };
 
