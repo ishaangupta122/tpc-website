ed844ddd664a458239430b084d2e327e60042446
Ishaan Gupta <ishaang2209@gmail.com>
2025-03-30 22:21:22 +0530

    added welcome animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,34 @@ import NotFound from "./components/NotFound";
 import ScrollToTop from "./components/ScrollToTop";
 import MessageBox from "./components/MsgBox";
 import { routes } from "./routes/Routes";
+import { useEffect, useState } from "react";
+import WelcomeLoading from "./components/WelcomeLoading";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <WelcomeLoading />;
+  }
+
   return (
-    <>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Navbar />
-        <MessageBox />
-        <Routes>
-          {routes.map(({ path, element }, index) => (
-            <Route key={index} path={path} element={element} />
-          ))}
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
+      <MessageBox />
+      <Routes>
+        {routes.map(({ path, element }, index) => (
+          <Route key={index} path={path} element={element} />
+        ))}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
